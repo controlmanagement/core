669dcf0142ebc6b01fcf3b951acb7cd2711ea9dc
H.Iwamura <h.iwamura@a.phys.nagoya-u.ac.jp>
2016-08-04 17:31:02 +0900

    Update controller.py

--- controller.py
+++ controller.py
@@ -330,6 +330,7 @@ class controller(object):
         lst_mm = int(lst_plb)
         lst_plb = 60.0*(lst_plb -lst_mm)
         lst_ss = int(lst_plb)
+        lst = str(lst_hh)+":"+str(lst_plb)+":"+str(lst_ss)
         
         if ant_status[1][0] & ant_status[1][1] == 1:
             drive_ready_az = 'ON'
@@ -396,7 +397,7 @@ class controller(object):
                    "None" : 'None',
                    "Current_M2" : beam_status[2],
                    "MJD" : mjd,
-                   "LST" : [lst_hh:lst_mm:lst_ss],
+                   "LST" : lst,
                    "Secofday" : secofday
                    }
                    
